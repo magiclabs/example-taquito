6b90adec220e8ebbc211b3590981e0031fad2e83
Hunter Cote <huntercote2@gmail.com>
2023-08-07 12:21:13 -0400

    linter

--- src/App.js
+++ src/App.js
@@ -1,126 +1,130 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { TezosToolkit } from '@taquito/taquito';
-import { Magic } from "magic-sdk";
-import { TaquitoExtension } from '@magic-ext/taquito'
-import "./styles.css";
+import { Magic } from 'magic-sdk';
+import { TaquitoExtension } from '@magic-ext/taquito';
+import './styles.css';
 
-const magic = new Magic("pk_live_C20A087BDF08A859", {
-    extensions: {
-        taquito: new TaquitoExtension({
-            rpcUrl: "https://ghostnet.tezos.marigold.dev"
-        })
-    }
+const magic = new Magic('pk_live_C20A087BDF08A859', {
+  extensions: {
+    taquito: new TaquitoExtension({
+      rpcUrl: 'https://ghostnet.tezos.marigold.dev',
+    }),
+  },
 });
 
 export default function App() {
-    const [email, setEmail] = useState("");
-    const [publicAddress, setPublicAddress] = useState("");
-    const [destinationAddress, setDestinationAddress] = useState("");
-    const [sendXTZAmount, setSendXTZAmount] = useState(0);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userMetadata, setUserMetadata] = useState({});
-    const [sendingTransaction, setSendingTransaction] = useState(false);
+  const [email, setEmail] = useState('');
+  const [publicAddress, setPublicAddress] = useState('');
+  const [destinationAddress, setDestinationAddress] = useState('');
+  const [sendXTZAmount, setSendXTZAmount] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userMetadata, setUserMetadata] = useState({});
+  const [sendingTransaction, setSendingTransaction] = useState(false);
 
-    useEffect(() => {
-        magic.user.isLoggedIn().then(async magicIsLoggedIn => {
-            setIsLoggedIn(magicIsLoggedIn);
-            if (magicIsLoggedIn) {
-                const publicAddress = (await magic.taquito.getPublicKey()).pkh;
-                setPublicAddress(publicAddress);
-                setUserMetadata(await magic.user.getMetadata());
-            }
-        });
-    }, [isLoggedIn]);
+  useEffect(() => {
+    magic.user.isLoggedIn().then(async magicIsLoggedIn => {
+      setIsLoggedIn(magicIsLoggedIn);
+      if (magicIsLoggedIn) {
+        const publicAddress = (await magic.taquito.getPublicKey()).pkh;
+        setPublicAddress(publicAddress);
+        setUserMetadata(await magic.user.getMetadata());
+      }
+    });
+  }, [isLoggedIn]);
 
-    const login = async () => {
-        await magic.auth.loginWithEmailOTP({ email });
-        setIsLoggedIn(true);
-    };
+  const login = async () => {
+    await magic.auth.loginWithEmailOTP({ email });
+    setIsLoggedIn(true);
+  };
 
-    const logout = async () => {
-        await magic.user.logout();
-        setIsLoggedIn(false);
-    };
+  const logout = async () => {
+    await magic.user.logout();
+    setIsLoggedIn(false);
+  };
 
-    const handleMagicSign = async () => {
-        setSendingTransaction(true);
-        const Tezos = new TezosToolkit('https://ghostnet.tezos.marigold.dev');
-        const magicSigner = await magic.taquito.createMagicSigner();
+  const handleMagicSign = async () => {
+    setSendingTransaction(true);
+    const Tezos = new TezosToolkit('https://ghostnet.tezos.marigold.dev');
+    const magicSigner = await magic.taquito.createMagicSigner();
 
-        Tezos.setProvider({signer: magicSigner});
+    Tezos.setProvider({ signer: magicSigner });
 
-        const operation = await  Tezos.contract.transfer({ to: destinationAddress, amount: sendXTZAmount })
+    const operation = await Tezos.contract.transfer({ to: destinationAddress, amount: sendXTZAmount });
 
-        setSendingTransaction(false);
-        console.log('result', operation)
-    }
+    setSendingTransaction(false);
+    console.log('result', operation);
+  };
 
-    return (
-        <div className="App">
-            {!isLoggedIn ? (
-                <div className="container">
-                    <h1>Please sign up or login</h1>
-                    <input
-                        type="email"
-                        name="email"
-                        required="required"
-                        placeholder="Enter your email"
-                        onChange={event => {
-                            setEmail(event.target.value);
-                        }}
-                    />
-                    <button onClick={login}>Send</button>
-                </div>
-            ) : (
-                <div>
-                    <div className="container">
-                        <h1>Current user: {userMetadata.email}</h1>
-                        <button onClick={logout}>Logout</button>
-                    </div>
-                    <div className="container">
-                        <h1>Tezos address</h1>
-                        <div className="info">
-                            {publicAddress}
-                        </div>
-                    </div>
-                    <div className="container">
-                        <h1>Send Transaction</h1>
-                        {
-                            sendingTransaction ?
-                                <div>
-                                    <div>
-                                        Send transaction success
-                                    </div>
-                                </div>
-                                :
-                                <div/>
-                        }
-                        <input
-                            type="text"
-                            name="destination"
-                            className="full-width"
-                            required="required"
-                            placeholder="Destination address"
-                            onChange={event => {
-                                setDestinationAddress(event.target.value);
-                            }}
-                        />
-                        <input
-                            type="text"
-                            name="amount"
-                            className="full-width"
-                            required="required"
-                            placeholder="Amount in XTZ"
-                            onChange={event => {
-                                setSendXTZAmount(event.target.value);
-                            }}
-                        />
-                        <button id="btn-send-txn" onClick={handleMagicSign}>
-                            Send Transaction
-                        </button>
-                    </div>
-                </div>
-            )}
+  return (
+    <div className="App">
+      {!isLoggedIn ? (
+        <div className="container">
+          <h1>Please sign up or login</h1>
+          <input
+            type="email"
+            name="email"
+            required="required"
+            placeholder="Enter your email"
+            onChange={event => {
+              setEmail(event.target.value);
+            }}
+          />
+          <button onClick={login}>Send</button>
         </div>
-    );
+      ) : (
+        <div>
+          <div className="container">
+            <h1>Current user: {userMetadata.email}</h1>
+            <button onClick={logout}>Logout</button>
+          </div>
+          <div className="container">
+            <h1>Tezos address</h1>
+            <div className="info">
+              <a href={`https://ghost.tzstats.com/${publicAddress}`} target="_blank" rel="noreferrer">
+                {publicAddress}
+              </a>
+            </div>
+            <button>
+              <a href="https://faucet.ghostnet.teztnets.xyz/" target="_blank" rel="noreferrer">
+                Faucet
+              </a>
+            </button>
+          </div>
+          <div className="container">
+            <h1>Send Transaction</h1>
+            {sendingTransaction ? (
+              <div>
+                <div>Sending transaction...</div>
+              </div>
+            ) : (
+              <div />
+            )}
+            <input
+              type="text"
+              name="destination"
+              className="full-width"
+              required="required"
+              placeholder="Destination address"
+              onChange={event => {
+                setDestinationAddress(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              name="amount"
+              className="full-width"
+              required="required"
+              placeholder="Amount in XTZ"
+              onChange={event => {
+                setSendXTZAmount(event.target.value);
+              }}
+            />
+            <button id="btn-send-txn" onClick={handleMagicSign}>
+              Send Transaction
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
